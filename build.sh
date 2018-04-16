ab2ef9f3f2861a4a71bc3ab66d38d33356922473
Nena Furtmeier <Nena@Furtmeier.IT>
2018-04-16 21:04:51 +0200

    added mac release

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 #electron-packager . --all --icon "/home/nemiah/NetBeansProjects/supportBoxElectron/iconWindows.ico"
 electron-packager . --overwrite --asar --platform=linux --arch=x64 --icon="/home/nemiah/NetBeansProjects/supportBoxElectron/iconLinux.png" --prune=true --out=release
 electron-packager . --overwrite --asar --platform=win32 --arch=ia32 --icon="/home/nemiah/NetBeansProjects/supportBoxElectron/iconWindows.ico" --prune=true --out=release
+electron-packager . --overwrite --asar --platform=darwin --arch=x64 --icon="/home/nemiah/NetBeansProjects/supportBoxElectron/iconMac.icns" --prune=true --out=release
